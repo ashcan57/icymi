bf5d2ef1ef7c248a14db8fa87d6d665983420c28
Richard Head <d3ltv4ce@yahoo.ca>
2025-10-21 06:31:43 -0400

    re redo

--- build_repo.py
+++ build_repo.py
@@ -40,13 +40,19 @@ def build_repo(base_path='.'):
         zip_addon(addon_path, zip_path)
         with open(addon_xml_file, 'r', encoding='utf-8') as f:
             addons_xml_parts.append(f.read().strip())
-    addons_xml = '<?xml version="1.0" encoding="UTF-8"?>\n<addons>\n' + '\n'.join(addons_xml_parts) + '\n</addons>'
+        # Build the repository XML
+    addons_xml = (
+        '<?xml version="1.0" encoding="UTF-8"?>\n'
+        '<addons>\n' +
+        '\n'.join(addons_xml_parts) +
+        '\n</addons>'
+    )
+
     with open(os.path.join(base_path, 'addons.xml'), 'w', encoding='utf-8') as f:
         f.write(addons_xml)
+
     md5 = hashlib.md5(addons_xml.encode('utf-8')).hexdigest()
     with open(os.path.join(base_path, 'addons.xml.md5'), 'w', encoding='utf-8') as f:
         f.write(md5)
     print("Built addons.xml and md5, and created addon zip files.")
 
-if __name__ == '__main__':
-    build_repo('.')
